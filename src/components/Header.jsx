49e4e16af091de25ffd9d1e686bf0d44de6ad783
nithishamal <nithishamal98@gmail.com>
2025-06-12 19:23:05 +0530

    Start

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header(){
+    return (
+        <>
+            <div className="container-title">
+                <h1>To Do List</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header;
